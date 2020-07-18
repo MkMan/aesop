c5a9e8a3836dc0a0b6c1c779432732e41d2d35f6
Mustafa Mansour <mustafa.k.mansour@gmail.com>
2020-07-18 11:08:13 +1000

    add Bootstrap

--- public/index.tsx
+++ public/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css"; // Import precompiled Bootstrap css
 import "./style.scss";
 
 const App = () => <div>Hello World</div>;
